14a11422d0d7008334666de95a90c88c04efed5e
01zulfi <85733202+01zulfi@users.noreply.github.com>
2025-08-07 11:08:49 +0500

    core: add support for standard notes' export files Signed-off-by: 01zulfi <85733202+01zulfi@users.noreply.github.com>

--- packages/core/src/providers/provider-factory.ts
+++ packages/core/src/providers/provider-factory.ts
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// import { StandardNotes } from "./standard-notes";
+import { StandardNotes } from "./standard-notes";
 import { Evernote } from "./evernote";
 import { Markdown } from "./md";
 import { HTML } from "./html";
@@ -40,7 +40,7 @@ const providerMap = {
   keep: GoogleKeep,
   simplenote: Simplenote,
   // onenote: OneNote,
-  // standardnotes: StandardNotes,
+  standardnotes: StandardNotes,
   zohonotebook: ZohoNotebook,
   joplin: Joplin,
   textbundle: TextBundle,
